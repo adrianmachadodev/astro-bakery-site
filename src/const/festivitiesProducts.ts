047dae3e2af3ac933e77df773f9edf9d3505c2db
Adrian Machado <machadoadriandev@gmail.com>
2025-08-24 13:13:01 -0300

    section festivities

---
+++ src/const/festivitiesProducts.ts
@@ -0,0 +1,12 @@
+import { imageConfig } from "astro:assets";
+
+export const FESTIVITIES = [
+  {
+    name: "Pastelitos de Dulce de Leche",
+    image: "public/images/Festivities/festivitie-1.webp",
+  },
+  {
+    name: "Pastelitos de Membrillo/batata",
+    image: "public/images/Festivities/festivitie-2.webp",
+  },
+];
