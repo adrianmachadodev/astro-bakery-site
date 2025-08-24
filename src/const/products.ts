11d6292384a3e5731a8ec8a33e8a183acb099c93
Adrian Machado <machadoadriandev@gmail.com>
2025-08-23 23:39:29 -0300

    section more product and festivities

---
+++ src/const/products.ts
@@ -0,0 +1,23 @@
+export const PRODUCTS = [
+  {
+    name: "Torta tofi",
+    description:
+      "Una combinación irresistible de bizcochuelo húmedo, relleno con una suave crema de dulce de leche y chocolate, coronada con una capa brillante de toffee artesanal que se derrite en cada bocado. Su sabor equilibra lo dulce y lo intenso, convirtiéndola en la elección perfecta para los amantes del chocolate y el caramelo.",
+    image:
+      "https://cloudfront-us-east-1.images.arcpublishing.com/canal26/CUAK7ZIMLJF3DNWH2V3QMD7KKY.jpg",
+  },
+  {
+    name: "Cheesecake",
+    description:
+      "La suavidad de un relleno cremoso de queso sobre una base crujiente de galletitas doradas, cubierto con una fina capa de fruta fresca o un delicado baño de salsa dulce que potencia su sabor único. Un postre que combina lo refinado y lo irresistible, perfecto para darse un gusto en cualquier ocasión.",
+    image:
+      "https://peopleenespanol.com/thmb/8vzve3hmeCgC-SVqkvAmuXnMMBE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/cheesecake-facil-con-leche-condensada-2000-4160526441114bf3ad8f3409586a2c8a.jpg",
+  },
+  {
+    name: "Lemon Pie",
+    description:
+      "Una base crujiente de galletitas doradas o masa suave, cubierta con una intensa y sedosa crema de limón que equilibra lo ácido y lo dulce a la perfección, coronada con un merengue esponjoso y dorado que se deshace en la boca. Fresco, ligero y simplemente irresistible.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEObPf7fPMgH77LCt6hfQJFc4ERdQFSn5i8A&s",
+  },
+];
