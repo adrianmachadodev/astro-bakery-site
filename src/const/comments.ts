5dcfc3075925c946af422934d32aafb6ad9202ea
Adrian Machado <machadoadriandev@gmail.com>
2025-08-28 21:57:50 -0300

    hero grid images and testimonials section

--- src/const/comments.ts
+++ src/const/comments.ts
@@ -1,26 +1,32 @@
 export const COMMENTS = [
-    {
-        user: 'Cristina M.',
-        comment: 'Todo muy rico!'
-    },
-    {
-        user: 'Cristina M.',
-        comment: 'Todo muy rico!'
-    },
-    {
-        user: 'Cristina M.',
-        comment: 'Todo muy rico!'
-    },
-    {
-        user: 'Cristina M.',
-        comment: 'Todo muy rico!'
-    },
-    {
-        user: 'Cristina M.',
-        comment: 'Todo muy rico!'
-    },
-    {
-        user: 'Cristina M.',
-        comment: 'Todo muy rico!'
-    },
-]
+  {
+    initialName: "CM",
+    user: "Cristina M.",
+    comment: "Muy buenos !!!!",
+  },
+  {
+    initialName: "AD",
+    user: "Arte Dulce",
+    comment: "Que ricooo!!!",
+  },
+  {
+    initialName: "MJ",
+    user: "Maria J.",
+    comment: "Que pinta!",
+  },
+  {
+    initialName: "DA",
+    user: "DemilAmores",
+    comment: "Riquisimos",
+  },
+  {
+    initialName: "CM",
+    user: "Cristina M.",
+    comment: "Todo muy rico!",
+  },
+  {
+    initialName: "CM",
+    user: "Cristina M.",
+    comment: "Todo muy rico!",
+  },
+];
