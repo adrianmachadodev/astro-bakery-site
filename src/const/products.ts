2d6af25d8bcb49d82964acf6d9a3de4a215f5012
Adrian Machado <machadoadriandev@gmail.com>
2025-09-14 13:19:00 -0300

    products details and footer

--- src/const/products.ts
+++ src/const/products.ts
@@ -9,15 +9,15 @@ export const PRODUCTS = [
   {
     name: "Cheesecake",
     description:
-      "La suavidad de un relleno cremoso de queso sobre una base crujiente de galletitas doradas, cubierto con una fina capa de fruta fresca o un delicado baño de salsa dulce que potencia su sabor único. Un postre que combina lo refinado y lo irresistible, perfecto para darse un gusto en cualquier ocasión.",
+      "La suavidad de un relleno cremoso de queso sobre una base crujiente de galletitas, cubierto con una fina capa de fruta fresca o un delicado baño de salsa dulce que potencia su sabor único. Un postre que combina lo refinado y lo irresistible, perfecto para darse un gusto en cualquier ocasión.",
     image:
       "https://peopleenespanol.com/thmb/8vzve3hmeCgC-SVqkvAmuXnMMBE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/cheesecake-facil-con-leche-condensada-2000-4160526441114bf3ad8f3409586a2c8a.jpg",
   },
   {
     name: "Lemon Pie",
     description:
-      "Una base crujiente de galletitas doradas o masa suave, cubierta con una intensa y sedosa crema de limón que equilibra lo ácido y lo dulce a la perfección, coronada con un merengue esponjoso y dorado que se deshace en la boca. Fresco, ligero y simplemente irresistible.",
+      "Una base crujiente de una masa suave, cubierta con una intensa y sedosa crema de limón que equilibra lo ácido y lo dulce a la perfección, coronada con un merengue esponjoso que se deshace en la boca. Fresco, ligero y simplemente irresistible.",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEObPf7fPMgH77LCt6hfQJFc4ERdQFSn5i8A&s",
+      "https://www.tasteofhome.com/wp-content/uploads/2018/01/World-s-Best-Lemon-Pie_EXPS_FT23_4439_JR_1129_1.jpg",
   },
 ];
